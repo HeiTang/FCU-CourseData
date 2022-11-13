033f8ff1cfb7d2c3d0f7bb7e7822e2e62c09057e
HeiTang <heitang.me@gmail.com>
2022-11-14 00:18:43 +0800

    refactor: Coursedump.py OOP.

--- Coursedump.py
+++ Coursedump.py
@@ -1,68 +1,83 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
-import requests,os,sys
-import json
+import requests
+import json, os, sys
 from bs4 import BeautifulSoup
 
-# 設定 Header 
-headers = {
-    "Accept": "*/*",
-    "Accept-Language": 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6',
-    "DNT": "1",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "Content-Type":"application/json; charset=UTF-8",
-    "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3573.0 Safari/537.36",
-    }
+SMS = [1, 2, 3, 4]    # 1:上學期 2:下學期 3:暑修上 4:暑修下
+DEGREE = [1, 3, 4, 5] # 1:大學部 3:碩士班 4:博士班 5:進修班
 
-
-def PostGetType1Result(path,year,sms,degree,deptId):
-
-    payload = {
-        "baseOptions":{
-            "lang":"cht",     #cht:中文 en:英文
-            "year":year,
-            "sms":sms         # 1:上學期 2:下學期 3:暑修上 4:暑修下
-        },
-        "typeOptions":{
-            "degree":degree,     # 1:大學 3:碩士 4:博士 5:進修    
-            "deptId":deptId,     # OD:跨領域設計學院(籌備 CC:創能學院 GE:通識中心 CA:工程與科學學院 CB:商學院 CH:人社學院 CI:資電學院 CD:建設學院 CF:金融學院 NM:國際科技與管理學院 AS:建築專業學院 PC:學分學程 XA:外語文 XC:通識核心課 XD:體育選項課 XE:綜合班 XF:統籌科目 XH:軍訓
-            "unitId":"*",
-            "classId":"*"
+class CourseDump:
+    def __init__(self, year, sms):
+        self.headers = {
+            "Content-Type":"application/json; charset=UTF-8",
+            "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3573.0 Safari/537.36",
         }
-    }
 
-    payload = json.dumps(payload)
-    url = "https://coursesearch04.fcu.edu.tw/Service/Search.asmx/GetType1Result"
+        self.payload = {
+            "baseOptions": {
+                "lang": "cht",
+                "year": year,
+                "sms": sms
+            }
+        }
 
-    r = requests.post(url,headers=headers,data = payload)  
-    r = r.text                                             
+    def getDeptList(self):
+        self.payload["degree"] = "1"
+        url = 'https://coursesearch03.fcu.edu.tw/Service/Search.asmx/GetDeptList'
+        r = requests.post(url, headers = self.headers, data = json.dumps(self.payload))
+        r = self.stringProcess(r.text)
+        return json.loads(r)
 
-    r = r.replace('\\"','"' )            # 將 \" 過濾成 "
-    r = r.replace('\\\"','"')            # 將 \\" 過濾成 "
-    r = r.replace('"d":"{','"d": {' )    
-    r = r.replace(']}"}',']}}')
+    def getCourseList(self, degree, dept_id):
+        self.payload["typeOptions"] = {
+            "degree": degree,
+            "deptId": dept_id,
+            "unitId": "*",
+            "classId": "*"
+        }
 
-    r = json.loads(r)                                                            # JSON -> dict
-    r = json.dumps(r , ensure_ascii=False , indent=4 , separators=(',', ': '))   # dict -> JSON
+        url = "https://coursesearch04.fcu.edu.tw/Service/Search.asmx/GetType1Result"
+        r = requests.post(url, headers = self.headers, data = json.dumps(self.payload))
+        r = self.stringProcess(r.text)
+        r = json.loads(r)                                                            # JSON -> dict
+        r = json.dumps(r, ensure_ascii = False, indent = 4, separators=(',', ': '))  # dict -> JSON
+        return r
 
-    name = str(year) + str(sms) + "-" + str(degree) + "-" + deptId + ".json"
-    print(name)
+    def saveFile(self, data, path, file_name):
+        with open(os.path.join(path, file_name), 'w') as f:
+            f.write(data)
+            f.flush()
+            f.close() 
 
-    with open(os.path.join(path,  name), 'w') as fo:
-        # fo = open(name, "w")
-        fo.write(str(r))
-        fo.flush()
-        fo.close() 
+    def stringProcess(self, string):
+        string = string.replace('\\"', '"' )            # 將 \" 過濾成 "
+        string = string.replace('\\\"', '"')            # 將 \\" 過濾成 "
+        string = string.replace('"d":"{', '"d": {' )   
+        string = string.replace('"d":"[{', '"d": [{' )
+        string = string.replace(']}"}', ']}}')
+        string = string.replace('}]"}', '}]}')
+        return string
 
 if __name__ == '__main__': 
     years = sys.argv[1].split(',')
-    for year in years:  
-        path = os.getcwd() + '/' +str(year)
+    for year in years:
+        # 確認資料夾是否存在，否則建立學年度資料夾
+        path = '{}/{}'.format(os.getcwd(), str(year))
         if not os.path.exists(path):
             os.mkdir(path)
-        for sms in 1 , 2 , 3 , 4 :
-            for degree in 1 , 3 , 4 , 5 :
-                for deptId in "OD" , "CC" , "GE" , "CA" , "CB" , "CH" , "CI" , "CD" , "CF" , "NM" , "AS" , "PC" , "XA" , "XC" , "XD" , "XE" , "XF" , "XH":
-                    PostGetType1Result(path,year,sms,degree,deptId)
+    
+        for sms in SMS:
+            course = CourseDump(year, sms)
+            dept = course.getDeptList()['d']
+
+            if dept != '[]':                     # 確認是否有開放查詢
+                for degree in DEGREE:
+                    for i in range(len(dept)):
+                        dept_id = dept[i]['id']
+                        data = course.getCourseList(degree, dept_id)
+                        file_name = '{}{}-{}-{}.json'.format(str(year), str(sms), str(degree), dept_id)
+                        course.saveFile(data, path, file_name)
+                        print(f"+ {file_name}")
+            else:
+                print("- {}{}: No data".format(str(year), str(sms)))
